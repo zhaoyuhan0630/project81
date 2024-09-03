4b3103135e8dce0398b73aba7a4acd25577d8815
liouschengga <128068105+zhaoyuhan0630@users.noreply.github.com>
2024-09-03 15:38:58 -0400

    Update drawstuff.js

--- drawstuff.js
+++ drawstuff.js
@@ -90,7 +90,7 @@ function drawRandPixels(context) {
 //get the input triangles from the standard class URL
 function getInputTriangles() {
     const INPUT_TRIANGLES_URL = 
-        "https://ncsucgclass.github.io/prog1/triangles.json";
+        "https://ncsucgclass.github.io/prog1/triangles2.json";
         
     // load the triangles file
     var httpReq = new XMLHttpRequest(); // a new http request
